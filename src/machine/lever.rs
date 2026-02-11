5504c93bdc2771495483caba7e3c08ce19e55997
mintylotl <mintyaxolotl@proton.me>
2026-02-11 22:27:10 +0200

    adjust droprates

--- src/machine/lever.rs
+++ src/machine/lever.rs
@@ -13,16 +13,16 @@ pub fn pull(pity_stats: &PityCtx) -> Rarities {
 
     let mut mythic = 0.00072;
     let mut s = 0.048;
-    let mut a = 0.072;
+    let mut a = 0.12;
 
     if pity_stats.sss_pity > 1104 {
         mythic += ((pity_stats.sss_pity - 1104) as f64 * 0.000024).min(0.01);
     }
-    if pity_stats.s_pity > 24 {
-        s += ((pity_stats.s_pity - 16) as f64 * 0.024).min(0.072);
+    if pity_stats.s_pity > 12 {
+        s += ((pity_stats.s_pity - 12) as f64 * 0.024).min(0.072);
     }
-    if pity_stats.a_pity > 12 {
-        a += ((pity_stats.a_pity - 12) as f64 * 0.048).min(0.24);
+    if pity_stats.a_pity > 4 {
+        a += ((pity_stats.a_pity - 4) as f64 * 0.048).min(0.24);
     }
 
     // 0.072% of hitting Mythic
